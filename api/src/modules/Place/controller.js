58b135fae238f8f9f825ae6ab9f41b614c84e94c
Louis Lagrange <lagrange.louis@gmail.com>
2019-04-04 11:37:09 +0200

    Add searchPlaces query

--- api/src/modules/Place/controller.js
+++ api/src/modules/Place/controller.js
@@ -6,4 +6,8 @@ export class PlaceController {
   static async findAll(ctx: ContextType): Promise<PlaceType[]> {
     return Place.find();
   }
+
+  static async search(query: string, ctx: ContextType): Promise<PlaceType[]> {
+    return Place.find({ take: 10 });
+  }
 }
--- api/src/modules/Place/resolver.js
+++ api/src/modules/Place/resolver.js
@@ -6,5 +6,7 @@ export default {
   Query: {
     allPlaces: (_: void, args: void, ctx: ContextType): Promise<PlaceType[]> =>
       PlaceController.findAll(ctx),
+    searchPlaces: (_: void, args: { query: string }, ctx: ContextType): Promise<PlaceType[]> =>
+      PlaceController.search(args.query, ctx),
   },
 };
